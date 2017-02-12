70493c2dd7fafaf3a76c96347e429652a05dcadf
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 14:00:16 -0500

    Add project files.

---
+++ MoreArrays/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MoreArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTwoDimensionalArray = new System.Windows.Forms.Button();
+            this.btnJaggedArray = new System.Windows.Forms.Button();
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.btnPractice = new System.Windows.Forms.Button();
+            this.btnDisplayArray = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTwoDimensionalArray
+            // 
+            this.btnTwoDimensionalArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwoDimensionalArray.Location = new System.Drawing.Point(12, 24);
+            this.btnTwoDimensionalArray.Name = "btnTwoDimensionalArray";
+            this.btnTwoDimensionalArray.Size = new System.Drawing.Size(214, 70);
+            this.btnTwoDimensionalArray.TabIndex = 0;
+            this.btnTwoDimensionalArray.Text = "Two-Dimensional Array";
+            this.btnTwoDimensionalArray.UseVisualStyleBackColor = true;
+            this.btnTwoDimensionalArray.Click += new System.EventHandler(this.btnTwoDimensionalArray_Click);
+            // 
+            // btnJaggedArray
+            // 
+            this.btnJaggedArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJaggedArray.Location = new System.Drawing.Point(255, 24);
+            this.btnJaggedArray.Name = "btnJaggedArray";
+            this.btnJaggedArray.Size = new System.Drawing.Size(200, 70);
+            this.btnJaggedArray.TabIndex = 1;
+            this.btnJaggedArray.Text = "Jagged Array";
+            this.btnJaggedArray.UseVisualStyleBackColor = true;
+            this.btnJaggedArray.Click += new System.EventHandler(this.btnJaggedArray_Click);
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Location = new System.Drawing.Point(12, 100);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(443, 332);
+            this.rtbDisplay.TabIndex = 2;
+            this.rtbDisplay.Text = "";
+            // 
+            // btnPractice
+            // 
+            this.btnPractice.Location = new System.Drawing.Point(12, 448);
+            this.btnPractice.Name = "btnPractice";
+            this.btnPractice.Size = new System.Drawing.Size(75, 23);
+            this.btnPractice.TabIndex = 3;
+            this.btnPractice.Text = "Pratice";
+            this.btnPractice.UseVisualStyleBackColor = true;
+            this.btnPractice.Click += new System.EventHandler(this.btnPractice_Click);
+            // 
+            // btnDisplayArray
+            // 
+            this.btnDisplayArray.Location = new System.Drawing.Point(338, 448);
+            this.btnDisplayArray.Name = "btnDisplayArray";
+            this.btnDisplayArray.Size = new System.Drawing.Size(117, 23);
+            this.btnDisplayArray.TabIndex = 4;
+            this.btnDisplayArray.Text = "Practice_Loop";
+            this.btnDisplayArray.UseVisualStyleBackColor = true;
+            this.btnDisplayArray.Click += new System.EventHandler(this.btnDisplayArray_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BackColor = System.Drawing.Color.White;
+            this.lblDisplay.Location = new System.Drawing.Point(107, 435);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(191, 36);
+            this.lblDisplay.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 499);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnDisplayArray);
+            this.Controls.Add(this.btnPractice);
+            this.Controls.Add(this.rtbDisplay);
+            this.Controls.Add(this.btnJaggedArray);
+            this.Controls.Add(this.btnTwoDimensionalArray);
+            this.Name = "Form1";
+            this.Text = "Two-Dimensional arrays and jagged arrays";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTwoDimensionalArray;
+        private System.Windows.Forms.Button btnJaggedArray;
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+        private System.Windows.Forms.Button btnPractice;
+        private System.Windows.Forms.Button btnDisplayArray;
+        private System.Windows.Forms.Label lblDisplay;
+    }
+}
+
---
+++ MoreArrays/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoreArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTwoDimensionalArray_Click(object sender, EventArgs e)
+        {
+            //declaring wihtout initialization
+            //decimal[,] price = new decimal [5,4];
+            //price[0, 0] = 450m;
+            //price[0, 1] = 450m;
+            //price[0, 2] = 540m;
+            //... ... ... shows you how to declare with initilization
+            //decimal stores more digits, more precise data
+            //used for money calculations
+            //m used to assign decimal numbers
+            decimal[,] prices =
+            {
+                {450m, 450m, 450m, 450m },
+                {425m, 425m, 425m, 425m },
+                {400m, 400m, 400m, 400m },
+                {375m, 375m, 375m, 375m },
+                {375m, 375m, 375m, 375m },
+                {350m, 350m, 350m, 350m }
+            };
+            rtbDisplay.Text = "traversgin a two-dimensional array, row by row: \n";
+            for (int x = 0; x <= prices.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= prices.GetUpperBound(1); y++)
+                {
+                    //rtbDisplay.AppendText(prices[x, y].ToString() + ",");
+                    lblDisplay.Text = (prices[x, y].ToString());
+                }
+                //rtbDisplay.AppendText(Environment.NewLine);
+                lblDisplay.Text.ToString();
+            }
+
+            rtbDisplay.AppendText("\ntraversing a two-dimensional array, column by column:\n");
+            for (int y = 0; y <= prices.GetUpperBound(1); y++)
+            {
+                for (int x = 0; x <= prices.GetUpperBound(0); x++)
+                {
+                    rtbDisplay.AppendText(prices[x, y].ToString() + ",");
+                }
+                rtbDisplay.AppendText(Environment.NewLine);
+            }
+            rtbDisplay.AppendText("\nThe sum of all items is:    ");
+            decimal sum = 0;
+            for (int y = 0; y <= prices.GetUpperBound(1); y++)
+            {
+                for (int x = 0; x <= prices.GetUpperBound(0); x++)
+                {
+                    sum += prices[x, y];
+                    //sum = sum + prices[x, y];
+                }
+            }
+            rtbDisplay.AppendText(sum.ToString());
+        }
+
+        private void btnJaggedArray_Click(object sender, EventArgs e)
+        {
+            //an array of 3 int array.
+            int[][] jaggedArray = new int[3][];//[rows][columns]
+
+            //create individual arrays to populate the jaged array.
+            jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
+            jaggedArray[1] = new int[3] { 5, 6, 7 };
+            jaggedArray[2] = new int[5] { 8, 9, 10, 11, 12 };
+
+            rtbDisplay.Text = "Below are elements from a jagged array: \n";
+            int sum = 0;
+            for (int x = 0; x <= jaggedArray.GetUpperBound(0); x++)//GetUpperBound(ro)
+            {
+                //for (int y=0; y < jaggedArray[x].Length; y++)
+                for (int y = 0; y <= jaggedArray[x].GetUpperBound(0); y++)
+                {
+                    rtbDisplay.AppendText(jaggedArray[x][y].ToString() + ",");
+                    sum += jaggedArray[x][y]; //add each element
+                }
+                rtbDisplay.AppendText("\nThe total is: " + sum.ToString());
+            }
+        }
+
+        private void btnPractice_Click(object sender, EventArgs e)
+        {
+            int[,] numbers =
+            {
+                {1,2,3,4,5 },
+                {6,7,8,9,10 },
+                {11,12,13,14,15 },
+                {16,17,18,19,20 },
+                {21,22,23,24,25 }
+            };
+            //rtbDisplay.AppendText("\ntraversing a two-dimensional array, DIAGONALLY:\n");
+            //for (int y = 0; y <= numbers.GetUpperBound(1); y++)
+            //{
+            //    for (int x = 0; x <= numbers.GetUpperBound(0); x++)
+            //    {
+            //        rtbDisplay.AppendText(numbers[x, y].ToString() + ",");
+            //    }
+            //    rtbDisplay.AppendText(Environment.NewLine);
+            //}
+            //rtbDisplay.AppendText("\nThe sum of DIAGONAL items is:    ");
+            //int sum = 0;
+            //for (int x = 0; x <= numbers.GetUpperBound(0); x++)
+            //{
+            //    sum += numbers[x, x];//you don't need Y, you ONLY NEED X
+            //    //x = (0, 0) (1, 1) (2, 2) etc. etc.
+            //}
+            //rtbDisplay.AppendText(sum.ToString());
+            lblDisplay.Text = numbers[4, 4].ToString();
+
+        }
+
+        private void btnDisplayArray_Click(object sender, EventArgs e)
+        {
+            int[,] numberDisplay = new int[5, 5];
+            int newNumbers = 1;
+            for (int x = 0; x <= numberDisplay.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= numberDisplay.GetUpperBound(1); y++)                            
+                {
+                    numberDisplay[x, y] = newNumbers;
+                    newNumbers++;
+                    rtbDisplay.AppendText(numberDisplay[x, y].ToString() + ",");
+                }
+                rtbDisplay.AppendText(Environment.NewLine);
+            }
+
+        }
+    }
+}
+
